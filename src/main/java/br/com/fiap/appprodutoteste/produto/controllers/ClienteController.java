c5c8403f1843e660060445b24c51cbc3ea9c8eee
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2022-04-13 21:40:15 -0300

    criacao da tela de detalhes de cliente e de produto

--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ClienteController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.fiap.appprodutoteste.produto.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -54,6 +56,20 @@ public class ClienteController {
 		
 		Cliente clienteEntity = modelMapper.map(cliente, Cliente.class);
 		clienteRepository.save(clienteEntity);
+		return new ModelAndView("redirect:/clientes");
+	}
+	
+	@GetMapping("clientes/{id}")
+	public ModelAndView mostrar(@PathVariable Long id) {
+		
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if(cliente.isPresent()) {
+			Cliente clienteGet = cliente.get();
+			ModelAndView modelView = new ModelAndView("clientes/detalhe");
+			modelView.addObject("cliente", clienteGet);
+			return modelView;
+		}
+		System.out.println("não encontrado!");
 		return new ModelAndView("redirect:/clientes");
 	}
 	
--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/HomeController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/HomeController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home(HttpServletRequest request) {
-		request.setAttribute("nomeUsuario", "Thiago Souza Xavier");
+		request.setAttribute("nomeUsuario", "Isabela Marques Dias");
 		
 		return "home";
 	}
